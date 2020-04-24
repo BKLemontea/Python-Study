d716a458eee5422103a402a26d001a0ea049a5c6
BKLemonTea <jyf1128@naver.com>
2020-04-24 20:24:03 +0900

    과제1

---
+++ 과제1.py
@@ -0,0 +1,16 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(180)
